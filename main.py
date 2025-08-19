39beb1a5f113e3c6083d1d53c4883f62c2441395
Arunraj6899 <sunil90272908@gmail.com>
2025-08-19 12:27:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\n🔥 Shivam Spam bot succusfully created ⚡\nMy Master ---> @Shivam_jaiswal0011")
+print("\n🔥 GOD FATHER Spam bot succusfully created ⚡\nMy Master ---> @_ll_GOD_FATHER_BABU_ll")
 
 
 async def main():
